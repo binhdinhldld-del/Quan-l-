074381251ff8bb37bb67273a4a6fd79d930c59f4
binhdinhldld-del <binhdinhldld@gmail.com>
2026-06-23 12:37:15 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/Quan-l- on main/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
